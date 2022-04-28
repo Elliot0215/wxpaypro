bdd9f0999f7f9fa4e76d59594c8c0b48af10a844
二月 <7889397+february_ge@user.noreply.gitee.com>
2022-04-28 17:50:28 +0800

    增加商品列表

--- src/components/QRCode.vue
+++ src/components/QRCode.vue
@@ -1,7 +1,42 @@
 <template>
   <div>
     <canvas id='QRCode_header'> </canvas>
-    <el-button style="display:block;margin:0 auto" @click='getQRCode()'>获取付款码</el-button>
+    <div>
+      <el-table
+        :data="tableData"
+        stripe
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+        :header-cell-style="{'text-align':'center'}"
+        :cell-style="{'text-align':'center'}">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column
+          prop="title"
+          label="商品名称"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="price"
+          label="价格(分)"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="updateTime"
+          label="更新时间"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="author"
+          label="作者">
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="button_get">
+      <el-button style="display:block;margin:0 auto" @click='getQRCode()'>获取付款码</el-button>
+    </div>
   </div>
 </template>
 
@@ -12,12 +47,29 @@ export default {
   name: 'QRCode',
   data () {
     return {
-      msg: '微信支付'
+      tableData: [],
+      selectIndex: ''
     }
   },
   methods: {
+    handleSelectionChange (selection) {
+      if (selection[0] !== undefined) {
+        this.selectIndex = selection[0].id
+      }
+    },
+
+    getTableData () {
+      let url = '/api/product/list'
+      this.tableData = []
+      var that = this
+      axios.get(url).then((reponse) => {
+        console.log(reponse)
+        that.tableData = reponse.data.data.productList
+      })
+    },
+
     getQRCode () {
-      let url = '/api/wxpay/native/4'
+      let url = '/api/wxpay/native/' + this.selectIndex
       axios.post(url).then((reponse) => {
         console.log(reponse)
         let opts = {
@@ -40,6 +92,9 @@ export default {
         })
       })
     }
+  },
+  created () {
+    this.getTableData()
   }
 }
 </script>
@@ -59,5 +114,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.button_get {
+  margin-top: 10px;
 }
 </style>
