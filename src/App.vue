1b814c5f4985384d1fd57934b0ebcca94b246597
二月 <7889397+february_ge@user.noreply.gitee.com>
2022-05-06 20:18:27 +0800

    增加导航菜单

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+}
+html,body,div,el-container {
+  margin: 0px;
+  padding: 0px;
 }
 </style>
--- src/components/QRCode.vue
+++ src/components/QRCode.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
+  <el-container>
+    <el-aside width="200px">菜单</el-aside>
+    <el-container>
+        <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+        </el-menu>
+      <el-main>
+        <div>
     <canvas id='QRCode_header'> </canvas>
     <div>
       <el-table
@@ -38,6 +66,10 @@
       <el-button style="display:block;margin:0 auto" @click='getQRCode()'>获取付款码</el-button>
     </div>
   </div>
+      </el-main>
+      <el-footer>浙ICP备20009552号 © 2022二月科技有限公司 <a href="https://beian.miit.gov.cn/" target="_blank" class="text-col-3 mx-2 blue">浙ICP备20009552号-7</a></el-footer>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -49,12 +81,24 @@ export default {
     return {
       tableData: [],
       selectIndex: '',
-      aleterErr: false
+      orderNo: ''
     }
   },
   methods: {
+    queryOrderStatus () {
+      let url = '/api/orderInfo/query-order-status/' + this.orderNo
+      axios.get(url).then((reponse) => {
+        console.log(reponse.data.message)
+        if (reponse.data.message === '成功' && reponse.data.code === 0) {
+          // alert('支付成功')
+          this.$message.success('支付成功')
+          clearInterval(this.timer)
+        }
+      })
+    },
     // 选择器事件处理
     handleSelectionChange (selection) {
+      console.log(selection)
       if (selection[0] !== undefined) {
         this.selectIndex = selection[0].id
       } else {
@@ -95,13 +139,18 @@ export default {
           }
         }
         this.QRCodeMsg = reponse.data.data.codeUrl
+        this.orderNo = reponse.data.data.orderNo
         let msg = document.getElementById('QRCode_header')
         QRCode.toCanvas(msg, this.QRCodeMsg, opts, function (error) {
           console.log(error)
         })
       })
+      this.timer = setInterval(() => {
+        this.queryOrderStatus()
+      }, 3000)
     }
   },
+  // 调用
   created () {
     this.getTableData()
   }
@@ -109,20 +158,36 @@ export default {
 </script>
 
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
-  color: #42b983;
+  text-decoration: none;
+}
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+body > .el-container {
+  margin-bottom: 40px;
+}
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 .button_get {
   margin-top: 10px;
